1f77df0e81a8d099c0e70e60aeb81750d145848a
Prashanth-AP <apprashanth11@gmail.com>
2024-11-20 10:01:12 +0530

    stage one data ingestion and artifacts

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "chicken-disease-classification-project"
-AUTHOR_USER_NAME = "Prashanth"
+AUTHOR_USER_NAME = "Prashanth-AP"
 SRC_REPO = "ChickendiseaseClassifier"
 AUTHOR_EMAIL = "apprashanth11@gmail.com"
 
