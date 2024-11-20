175e062d3c9b6f371e9d4801e29f2d1b1b326a19
Prashanth-AP <apprashanth11@gmail.com>
2024-11-20 20:35:55 +0530

    evaluation and local host completed

--- src/ChickendiseaseClassifier/config/configuration.py
+++ src/ChickendiseaseClassifier/config/configuration.py
@@ -5,6 +5,11 @@ from src.ChickendiseaseClassifier.entity.config_entity import DataIngestionConfi
 from src.ChickendiseaseClassifier.entity.config_entity import PrepareBaseModelConfig
 from src.ChickendiseaseClassifier.entity.config_entity import PrepareCallbacksConfig
 from src.ChickendiseaseClassifier.entity.config_entity import TrainingConfig
+from src.ChickendiseaseClassifier.entity.config_entity import EvaluationConfig
+from pathlib import Path
+
+
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -89,3 +94,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
