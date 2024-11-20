2e27043315a9aa123a90bf9a8b2dc6b395adf6f7
Prashanth-AP <apprashanth11@gmail.com>
2024-11-20 16:40:50 +0530

    training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.ChickendiseaseClassifier import logger
 from src.ChickendiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ChickendiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.ChickendiseaseClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -23,3 +24,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME="Training"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
